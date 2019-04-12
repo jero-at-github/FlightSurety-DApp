3287e19f798a303b6a860eb250d129d5fd66ef03
Jero <jlopeznavarro@gmail.com>
2019-04-12 15:14:34 +0200

    Replaced dom.js by Vue.js

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -1,8 +1,6 @@
 
-import DOM from './dom';
 import Contract from './contract';
 import './flightsurety.css';
-
 
 (async() => {
 
@@ -10,14 +8,22 @@ import './flightsurety.css';
 
     let contract = new Contract('localhost', () => {
 
+        var flights = new Vue({
+            el: '#app-2',
+            data: {
+              message: 'You loaded this page on ' + new Date().toLocaleString()
+            }
+        })
+
         // Read transaction
         contract.isOperational((error, result) => {
             console.log(error,result);
-            display('Operational Status', 'Check if contract is operational', [ { label: 'Operational Status', error: error, value: result} ]);
+            document.querySelector("#operationalStatus").innerHTML = result;            
         });
     
 
         // User-submitted transaction
+        /*
         DOM.elid('submit-oracle').addEventListener('click', () => {
             let flight = DOM.elid('flight-number').value;
             // Write transaction
@@ -25,28 +31,12 @@ import './flightsurety.css';
                 display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp} ]);
             });
         })
+        */
     
     });
     
 
 })();
-
-
-function display(title, description, results) {
-    let displayDiv = DOM.elid("display-wrapper");
-    let section = DOM.section();
-    section.appendChild(DOM.h2(title));
-    section.appendChild(DOM.h5(description));
-    results.map((result) => {
-        let row = section.appendChild(DOM.div({className:'row'}));
-        row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
-        row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
-        section.appendChild(row);
-    })
-    displayDiv.append(section);
-
-}
-
 
 
 
