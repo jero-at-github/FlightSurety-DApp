480b0ecc73995a923946013d5a7fd0b63a6407be
Jero <jlopeznavarro@gmail.com>
2019-04-15 14:52:39 +0200

    Fixed flights test

--- test/flights.js
+++ test/flights.js
@@ -21,37 +21,41 @@ contract('Flight Surety Flights Tests', async (accounts) => {
         });
 
         it('Flights registration', async () => {
-                       
-            let flight = null;
-          
+                                            
             // Register flights            
             for (let i = 0; i < config.flights.length; i ++) {               
 
+                let flight = config.flights[i];
+                let airline = config.airlines[config.flights[i].airlineIndex];
+
                 await config.flightSuretyApp.registerFlight(
-                    config.flights[i].description,
-                    config.flights[i].flightCode,
+                    flight.description,
+                    flight.flightCode,
                     Math.floor(Date.now() / 1000),
-                    config.flights[i].airline
+                    airline.address                    
                 );
             }                                        
         });       
 
         it('Passengers can buy a surety', async () => {                                  
           
+            let flight = config.flights[0];
+            let airline = config.airlines[config.flights[0].airlineIndex];
+
             // a passenger can buy a surety
             await config.flightSuretyApp.buySurety(
-                config.flights[0].description,
-                config.flights[0].flightCode,                
-                config.flights[0].airline, 
+                flight.description,
+                flight.flightCode,                
+                airline.address,
                 { from: config.passengers[0], value: SURETY_PRICE }
             );
            
             // a passenger can't buy twice the same surety            
             await truffleAssert.reverts(
                 config.flightSuretyApp.buySurety(
-                    config.flights[0].description,
-                    config.flights[0].flightCode,                
-                    config.flights[0].airline, 
+                    flight.description,
+                    flight.flightCode,                
+                    airline.address,
                     { from: config.passengers[0], value: SURETY_PRICE }
                 ), "revert " + "This passenger bought already a surety for this flight!"
             );           
